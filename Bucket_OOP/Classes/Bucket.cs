3e2963038be70645b6d255071384aa9433be919e
Rowan Brouwer <brouwerrowan@gmail.com>
2020-12-03 11:58:50 +0100

    Add project files.

---
+++ Bucket_OOP/Classes/Bucket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bucket_OOP.Classes
+{
+    sealed class Bucket : BucketBase
+    {
+        #region
+        private int _content { get; set; }
+        private int _capacity { get; set; }
+        #endregion
+
+
+        public Bucket(int content, int capacity) : base(content, capacity)
+        {
+            if (capacity >= content && capacity > 10 || capacity < 12)
+            {
+                this._content = content; this._capacity = capacity;
+            }
+            else
+            {
+                this.Capacity = 12; this.Content = 0;
+            }
+        }
+        protected override void fill(int amount)
+        {
+            
+        }
+    }
+}
---
+++ Bucket_OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bucket_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
